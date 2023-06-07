cc314335e5acfd109b1f6ab04ddf3cf6590f63d6
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2023-06-07 02:34:14 +0200

    Add files via upload

--- 2_Z2-Serializacja_Deserializacja/src/Z2_SD/Z2-Serializacja_Deserializacja_code/Z2-Serializacja_Deserializacja/Program.cs
+++ 2_Z2-Serializacja_Deserializacja/src/Z2_SD/Z2-Serializacja_Deserializacja_code/Z2-Serializacja_Deserializacja/Program.cs
@@ -4,46 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
 
 namespace Z2_Serializacja_Deserializacja
 {
-    internal class Program
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Osoba osoba = new Osoba("Dorota", 78); // utworzenie nowego obiektu Osoba
+            // Create a new Person object and set its properties
+            Person person = new Person
+            {
+                Name = "John Smith",
+                Age = 30
+            };
 
-            // poniżej następuje serializacja obiektu (czyli Osoba) do pliku
-            IFormatter formatowanie = new BinaryFormatter();
-            Stream strumienDanych = new FileStream("osoba.bin", FileMode.Create, FileAccess.Write);
-            formatowanie.Serialize(strumienDanych, osoba);
-            strumienDanych.Close();
+            // Serialize the Person object to JSON
+            string json = JsonConvert.SerializeObject(person);
 
-            // poniżej jest wykonana deserializacja obiektu (czyli Osoba) z pliku
-            strumienDanych = new FileStream("osoba.bin", FileMode.Open, FileAccess.Read);
-            Osoba deserializedPerson = (Osoba)formatowanie.Deserialize(strumienDanych);
-            strumienDanych.Close();
+            Console.WriteLine("Serialized JSON:");
+            Console.WriteLine(json);
 
-            // poniżej jest wyświetlenie właściwości naszego desierializowanego obiektu (Osoba)
-            Console.WriteLine("Imię osoby: " + deserializedPerson.Imie);
-            Console.WriteLine("Wiek osoby: " + deserializedPerson.Wiek);
+            // Deserialize the JSON string to a new Person object
+            Person deserializedPerson = JsonConvert.DeserializeObject<Person>(json);
+
+            Console.WriteLine("Deserialized Person:");
+            Console.WriteLine("Name: " + deserializedPerson.Name);
+            Console.WriteLine("Age: " + deserializedPerson.Age);
 
             Console.ReadLine();
         }
     }
-    public class Osoba
-    {
-        public string Imie { get; set; }
-        public int Wiek { get; set; }
-
-        public Osoba(string imie, int wiek)
-        {
-            Imie = imie;
-            Wiek = wiek;
-        }
-    }
-
 }
