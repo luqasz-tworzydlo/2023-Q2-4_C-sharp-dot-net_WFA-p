ed18846034915fbe1a154ad74752722f6286d0f7
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2023-06-09 00:38:15 +0200

    Add files via upload

---
+++ 5_Z5-PolishBrickBreaker/src/PBB/PolishBrickBreaker_code/PolishBrickBreaker/Score_Wynik.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PolishBrickBreaker
+{
+    public static class Score_Wynik
+    {
+        // metoda odnoszaca sie do calkowitego wyniku
+        public static int TotalScore_CalkowityWynik
+        {
+            get;
+            set;
+        }
+
+        // metoda odnoszaca sie do zdobywania wyniku
+        public static int GetScore_OtrzymajWynik
+        {
+            get
+            {
+                return TotalScore_CalkowityWynik;
+            }
+        }
+
+        // metoda odnoszaca sie do konca gry
+        public static bool GameOver_KoniecGry
+        {
+            get;
+            set;
+        }
+
+        // metoda odnoszaca sie do obliczania wyniku
+        public static void CalculateScore_ObliczWynik (PictureBox brick, PolishBrickBreaker form)
+        {
+            if (brick.BackColor == Color.Blue)
+                TotalScore_CalkowityWynik += 1;
+            else if (brick.BackColor == Color.Red)
+                TotalScore_CalkowityWynik += 2;
+            else if (brick.BackColor == Color.Purple)
+                TotalScore_CalkowityWynik += 3;
+            else if (brick.BackColor == Color.Yellow)
+                TotalScore_CalkowityWynik += 4;
+            else if (brick.BackColor == Color.Green)
+                TotalScore_CalkowityWynik += 5;
+            else // odnosi to sie do czarnego koloru
+                TotalScore_CalkowityWynik += 6;
+
+            // ponizsza instrukcja powoduje wypisanie wyniku
+            form.Text = "Score / Wynik: " + TotalScore_CalkowityWynik;
+        }
+    }
+}
