874cc037ad2f32c30ef4392ce025b5ae7bcfed69
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2023-06-09 00:42:56 +0200

    Add files via upload

--- 5_Z5-PolishBrickBreaker/src/PBB/PolishBrickBreaker_code/PolishBrickBreaker/Paddle_Plytka.cs
+++ 5_Z5-PolishBrickBreaker/src/PBB/PolishBrickBreaker_code/PolishBrickBreaker/Paddle_Plytka.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,11 +42,35 @@ namespace PolishBrickBreaker
         }
         // metoda odnoszaca sie do generowania (inicjowania) naszej plytki,
         // ktora skladac sie bedzie z trzech czesci [czesc lewa, czesc srodkowa,
-        // czesc prawa], ktora bedzie odbijac pileczke [ball], ktora bedziemy
-        // pozniej zbijac cegielki [bricks] na formularzu / planszy [form]
+        // czesc prawa], ktora bedzie poruszac i odbijac pileczke [ball],
+        // ktora bedziemy pozniej zbijac cegielki [bricks] na planszy [form]
         private void initialize_inicjowanie()
         {
+            // tworzymy trzy elementy PictureBox
+            // trzy czesci plyki [lewa, srodek, prawa]
+            for (int i = 0; i < 3; i++)
+            {
+                PlayerPaddles_PlytkiGracza.Add(new PictureBox()
+                {
+                    BackColor = Color.DarkOrange, // ustawienie koloru dla naszej plytki
+                    Height = 11, // ustawienie wysokosci naszej plyki [czyli u nas to 11]
+                    Visible = true, // ustawienie, aby nasza plytka byla widoczna na planszy
+                    Width = 30, // ustawienie szerokosci plytki [laczna szerokosc to bedzie 90]
+                    Top = form.ClientSize.Height - 11, // ustawienie polozenia naszej plytki na wysokosci
+                    Left = (form.ClientSize.Width - 30) / 2 // ustawienie naszej plytki na szerokosci
+                });
 
+                // odpowiednie ustawienie naszych trzech czesci plytek,
+                // czyli naszej lewej, srodkowej oraz prawej czesci
+                if (i == 0) // ustawienie lewej czesci plytki
+                    PlayerPaddles_PlytkiGracza[i].Left -= 30;
+                if (i == 2) // ustawienie prawej czesci plytki
+                    PlayerPaddles_PlytkiGracza[i].Left += 30;
+
+                // dodanie naszej plytki do naszej planszy [form],
+                // czyli dodanie trzech czesci naszej plytki
+                form.Controls.Add(PlayerPaddles_PlytkiGracza[i]);
+            }
         }
     }
 }
